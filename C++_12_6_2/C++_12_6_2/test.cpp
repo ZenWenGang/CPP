4532009c87e9ad5d75f379c359c7e6a9ffbbdb4f
zeng-wengang <2822445966@qq.com>
2025-12-06 20:54:04 +0800

    类和对象1.cpp

---
+++ C++_12_6_2/C++_12_6_2/test.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	void Init(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void Print()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	// 这⾥只是声明，没有开空间
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	// Date类实例化出对象d1和d2
+	Date d1;
+	Date d2;
+
+	d1.Init(2025, 11, 30);
+	d1.Print();
+	d2.Init(2025, 12, 31);
+	d2.Print();
+	return 0;
+}
+
