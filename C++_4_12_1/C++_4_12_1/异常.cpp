9b8f513da5c3974b7a58e74f72820e0403b7ae2c
ZenWenGang <2822445966@qq.com>
2026-04-12 17:25:31 +0800

    Add files via upload

---
+++ C++_4_12_1/C++_4_12_1/异常.cpp
@@ -0,0 +1,254 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<exception>
+using namespace std;
+
+//double Divide(int a, int b)
+//{
+//	try
+//	{
+//		// 当b == 0时抛出异常
+//		if (b == 0)
+//		{
+//			string s("Divide by zero condition!");
+//			throw s;
+//
+//			//cout << __FUNCTION__ << ":" << __LINE__ << "行执行" << endl;
+//		}
+//		else
+//		{
+//			return ((double)a / (double)b);
+//		}
+//	}
+//	catch (int errid)
+//	{
+//		cout << errid << endl;
+//	}
+//
+//	cout << __FUNCTION__ << ":" << __LINE__ << "行执行" << endl;
+//
+//	return 0;
+//}
+//
+//void Func()
+//{
+//	int len, time;
+//	cin >> len >> time;
+//
+//	try
+//	{
+//		cout << Divide(len, time) << endl;
+//	}
+//	catch (const char* errmsg)
+//	{
+//		cout << errmsg << endl;
+//	}
+//	cout << __FUNCTION__ << ":" << __LINE__ << "行执行" << endl;
+//}
+//
+//int main()
+//{
+//	while (1)
+//	{
+//		try
+//		{
+//			Func();
+//		}
+//		catch (const char* errmsg)
+//		{
+//			cout << errmsg << endl;
+//		}
+//		catch (...) //  任意类型的异常
+//		{
+//			cout << "未知异常" << endl;
+//		}
+//	}
+//	return 0;
+//}
+
+//double Divide(int a, int b)
+//{
+//	// 当b == 0时抛出异常
+//	if (b == 0)
+//	{
+//		throw "Divide by zero condition!";
+//	}
+//	else
+//	{
+//		return (double)a / (double)b;
+//	}
+//}
+//void Func()
+//{
+//	// 这⾥可以看到如果发⽣除0错误抛出异常，另外下⾯的array和array2没有得到释放。
+//	// 所以这⾥捕获异常后并不处理异常，异常还是交给外⾯处理，这⾥捕获了再重新抛出去。
+//	// 但是如果array2new的时候抛异常呢，就还需要套⼀层捕获释放逻辑，这⾥更好解决⽅案
+//	// 是智能指针，否则代码太挫了
+//
+//	int* array1 = new int[10];
+//	int* array2 = new int[10];   // 抛异常呢
+//
+//	try
+//	{
+//		int len, time;
+//		cin >> len >> time;
+//		cout << Divide(len, time) << endl;
+//	}
+//	catch (...)
+//	{
+//		cout << "delete []" << array1 << endl;
+//		cout << "delete []" << array2 << endl;
+//		delete[] array1;
+//		delete[] array2;
+//		throw; // 异常重新抛出，捕获到什么抛出什么
+//	}
+//
+//	cout << "delete []" << array1 << endl;
+//	delete[] array1;
+//	cout << "delete []" << array2 << endl;
+//	delete[] array2;
+//}
+//int main()
+//{
+//	try
+//	{
+//		Func();
+//	}
+//	catch (const char* errmsg)
+//	{
+//		cout << errmsg << endl;
+//	}
+//	catch (const exception& e)
+//	{
+//		cout << e.what() << endl;
+//	}
+//	catch (...)
+//	{
+//		cout << "未知异常" << endl;
+//	}
+//	return 0;
+//}
+
+//template<class T>
+//class SmartPtr
+//{
+//public:
+//    // RAII
+//    SmartPtr(T* ptr)
+//        :_ptr(ptr)
+//    {
+//    }
+//    ~SmartPtr()
+//    {
+//        cout << "delete[] " << _ptr << endl;
+//        delete[] _ptr;
+//    }
+//
+//    //重载运算符，模拟指针的⾏为，⽅便访问资源
+//    T& operator*()
+//    {
+//        return *_ptr;
+//    }
+//    T* operator->()
+//    {
+//        return _ptr;
+//    }
+//    T& operator[](size_t i)
+//    {
+//        return _ptr[i];
+//    }
+//private:
+//    T* _ptr;
+//};
+//
+//double Divide(int a, int b)
+//{
+//    if (b == 0)
+//    {
+//        throw "Divide by zero condition!";
+//    }
+//    else
+//    {
+//        return (double)a / (double)b;
+//    }
+//}
+//void Func()
+//{
+//    //这⾥使⽤RAII的智能指针类管理 new出来的数组以后，程序简单多了
+//    SmartPtr<int> sp1 = new int[10];
+//    SmartPtr<int> sp2 = new int[10];
+//
+//    for (size_t i = 0; i < 10; i++)
+//    {
+//        sp1[i] = sp2[i] = i;
+//    }
+//    int len, time;
+//    cin >> len >> time;
+//    cout << Divide(len, time) << endl;
+//}
+//int main()
+//{
+//    try
+//    {
+//        Func();
+//    }
+//    catch (const char* errmsg)
+//    {
+//        cout << errmsg << endl;
+//    }
+//    catch (const exception& e)
+//    {
+//        cout << e.what() << endl;
+//    }
+//    catch (...)
+//    {
+//        cout << "未知异常" << endl;
+//    }
+//    return 0;
+//}
+
+struct Date
+{
+	int _year;
+	int _month;
+	int _day;
+
+	Date(int year = 1, int month = 1, int day = 1)
+		:_year(year)
+		, _month(month)
+		, _day(day)
+	{
+	}
+
+	~Date()
+	{
+		cout << "~Date()" << endl;
+	}
+};
+
+int main()
+{
+	auto_ptr<Date> ap1(new Date);
+	// 拷贝时，管理权限转移，被拷贝对象ap1悬空
+	auto_ptr<Date> ap2(ap1);
+	//ap1->_year++;// 空指针访问err，ap1对象已经悬空
+
+	unique_ptr<Date> up1(new Date);
+	// 不支持拷贝
+	//unique_ptr<Date> up2(up1);
+	// 支持移动，但是移动后up1也悬空，所以使用移动要谨慎
+	//unique_ptr<Date> up3(move(up1));
+
+	shared_ptr<Date> sp1(new Date);
+	// 支持拷贝
+	shared_ptr<Date> sp2(sp1);
+	shared_ptr<Date> sp3(sp2);
+	cout <<"引用计数：" << sp1.use_count() << endl;
+	sp1->_year++;
+	cout << sp1->_year << endl;
+	cout << sp2->_year << endl;
+	cout << sp3->_year << endl;
+
+	return 0;
+}
