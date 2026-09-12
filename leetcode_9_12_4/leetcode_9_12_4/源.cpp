0bdb11d96e9dd4d2b135f75769f4414dd962eb0d
ZenWenGang <2822445966@qq.com>
2026-09-12 16:21:39 +0800

    Add files via upload

---
+++ leetcode_9_12_4/leetcode_9_12_4/源.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        if (x < 1) return 0;
+        int left = 1, right = x;
+        while (left < right)
+        {
+            long long mid = left + (right - left + 1) / 2;
+            if (mid * mid <= x)
+                left = mid;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+};
