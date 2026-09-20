d16b503dc6ac2079cba36930a2bab2b25e68b536
ZenWenGang <2822445966@qq.com>
2026-09-20 23:29:12 +0800

    Add files via upload

---
+++ leetcode_9_20_1/leetcode_9_20_1/源.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution
+{
+public:
+    int missingNumber(vector<int>& nums)
+    {
+        int ret = nums.size();
+        for (int i = 0; i < nums.size(); i++)
+            ret ^= i ^ nums[i];
+        return ret;
+    }
+};
